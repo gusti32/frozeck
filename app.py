25f6565384ad1941a4b6ff2e5dbaaee1d943f76b
gusti32 <if20.muhammadnurfathin@mhs.ubpkarawang.ac.id>
2024-01-07 23:32:16 +0700

    Page test

--- app.py
+++ app.py
@@ -5,4 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template('page.html.jinja')
+    return render_template('page.html')
+
+@app.route("/infer")
+def infer():
+    return 'Test'
+
+if __name__ == '__main__':
+    app.run()
