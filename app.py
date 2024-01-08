9e82232834eec67d08966c646ef6849536a8d3a3
gusti32 <if20.muhammadnurfathin@mhs.ubpkarawang.ac.id>
2024-01-08 21:45:07 +0700

    Add functionality to start and stop camera stream

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
+from os import environ
+
+CAMERA_URL = environ.get('CAMERA_URL')
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template('page.html')
+    return render_template('page.html', camera_url=CAMERA_URL)
 
 @app.route("/infer")
 def infer():
